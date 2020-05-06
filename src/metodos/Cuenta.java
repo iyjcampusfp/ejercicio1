b3569fb1250043183b87281b4171ef06a4a00220
iyjcampusfp <iyjcampusfp@gmail.com>
2020-05-06 19:30:13 +0200

    Comentarios de Métodos y Atributos

--- src/metodos/Cuenta.java
+++ src/metodos/Cuenta.java
@@ -1,21 +1,27 @@
 package metodos;
 
 public class Cuenta {
-    // Creación de Atributos
+                                    // Atributos
+    // Creación de Atributos (Titular de la Cuenta y Cantidad de dinero)
     private String titular;
     private double cantidad;
-    
-    // Constructores
+    /*****************************************************************************************/
+                            // Constructores (Dos Constructores)
+                    // Definición: Queremos que cantidad sea opcinal en el Constructor
+    /******************************************************************************************/
+    // 1º Constructor solo Un parámetro que será el titular
     public Cuenta( String titular ){
         this.titular = titular;
     }
     
+    // 2ºConstructor Dos parámetros , con la cantidad como opcional.
     public Cuenta( String titular,double cantidad ){
         this.titular = titular;
         this.cantidad = cantidad;
     }
-    
-    // Getters y Setters
+    /******************************************************************************************/
+                                // Getters y Setters
+    // Getters
     private String getTitular(){
         return this.titular;
     }
@@ -24,6 +30,7 @@ public class Cuenta {
         return this.cantidad;
     }
     
+    // Setters
     private void setTitular( String titular ){
         this.titular = titular;
     }
@@ -31,8 +38,9 @@ public class Cuenta {
     private void setCantidad( double cantidad ){
         this.cantidad = cantidad;
     }
-    
-    // Método toString
+    /**********************************************************************************************/
+                                            // Método toString
+    // Explicación:Realizamos un try y un catch , para comprobar si cantidad existe.
     @Override
     public String toString(){
         String textoADevolver;
@@ -45,4 +53,14 @@ public class Cuenta {
         
         return textoADevolver;
     }
+    /**********************************************************************************************/
+    // Métodos Personales
+    
+    
+    
+    
+    
+    
+    
+    
 }
