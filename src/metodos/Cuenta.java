1a6e3e8f9c8ec2e614f2f62826705ee3613e6c54
iyjcampusfp <iyjcampusfp@gmail.com>
2020-05-06 19:18:23 +0200

    Creación de métodos y atributos

--- src/metodos/Cuenta.java
+++ src/metodos/Cuenta.java
@@ -1,5 +1,48 @@
 package metodos;
 
 public class Cuenta {
-    //
+    // Creación de Atributos
+    private String titular;
+    private double cantidad;
+    
+    // Constructores
+    public Cuenta( String titular ){
+        this.titular = titular;
+    }
+    
+    public Cuenta( String titular,double cantidad ){
+        this.titular = titular;
+        this.cantidad = cantidad;
+    }
+    
+    // Getters y Setters
+    private String getTitular(){
+        return this.titular;
+    }
+    
+    private double getCantidad(){
+        return this.cantidad;
+    }
+    
+    private void setTitular( String titular ){
+        this.titular = titular;
+    }
+    
+    private void setCantidad( double cantidad ){
+        this.cantidad = cantidad;
+    }
+    
+    // Método toString
+    @Override
+    public String toString(){
+        String textoADevolver;
+        
+        try{
+            textoADevolver = this.titular + " tiene " + this.cantidad;
+        }catch(Exception e){
+            textoADevolver = "El titular es: " + this.titular;
+        }
+        
+        return textoADevolver;
+    }
 }
